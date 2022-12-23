8cc607d11653885c3714b035d073591320038d18
Federico <federicolucaspontoriero@gmail.com>
2022-12-23 13:21:29 -0300

    added fonts

--- whatsapp-clone/App.tsx
+++ whatsapp-clone/App.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback, useEffect, useState } from "react";
+import * as Font from "expo-font";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -9,7 +10,30 @@ export default function App() {
     const [appIsLoaded, setAppIsLoaded] = useState(false);
 
     useEffect(() => {
-        setAppIsLoaded(true);
+        const prepare = async () => {
+            try {
+                await Font.loadAsync({
+                    black: require("./assets/fonts//Roboto-Black.ttf"),
+                    blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+                    bold: require("./assets/fonts/Roboto-Bold.ttf"),
+                    boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+                    italic: require("./assets/fonts/Roboto-Italic.ttf"),
+                    light: require("./assets/fonts/Roboto-Light.ttf"),
+                    lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+                    medium: require("./assets/fonts/Roboto-Medium.ttf"),
+                    mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+                    regular: require("./assets/fonts/Roboto-Regular.ttf"),
+                    thin: require("./assets/fonts/Roboto-Thin.ttf"),
+                    thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+                });
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setAppIsLoaded(true);
+            }
+        };
+
+        prepare();
     }, []);
 
     const onLayout = useCallback(async () => {
@@ -35,5 +59,10 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+    },
+    label: {
+        color: "black",
+        fontSize: 18,
+        fontFamily: "regular",
     },
 });
