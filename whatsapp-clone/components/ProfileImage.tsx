21ba01166d630d30df38cba2333a4eb48636de43
Federico <federicolucaspontoriero@gmail.com>
2023-01-02 14:28:47 -0300

    requesting permission for images

--- whatsapp-clone/components/ProfileImage.tsx
+++ whatsapp-clone/components/ProfileImage.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
 import { FontAwesome } from "@expo/vector-icons"
 
 import colors from '../constants/colors'
+import { launchImagePicker } from '../utils/imagePickerHelper'
 
 const userImage = require('../assets/userImage.jpg')
 
 const ProfileImage = (props: any) => {
+  const pickImage = () => {
+    launchImagePicker()
+  }
+
   return (
-    <View>
+    <TouchableOpacity onPress={pickImage}>
       <Image
         source={userImage} style={{ ...styles.image, ...{ width: props.size, height: props.size } }} />
       <View style={styles.editIconContainer}>
         <FontAwesome name='pencil' size={15} color='black' />
       </View>
-    </View>
+    </TouchableOpacity>
   )
 }
 
---
+++ whatsapp-clone/utils/imagePickerHelper.ts
@@ -0,0 +1,15 @@
+import * as ImagePicker from 'expo-image-picker'
+import { Platform } from 'react-native'
+
+export const launchImagePicker = async () => {
+  await checkMediaPermissions();
+}
+
+const checkMediaPermissions = async () => {
+  if (Platform.OS !== 'web') {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (permissionResult.granted === false) return Promise.reject('We need permission to access you photos')
+  }
+
+  return Promise.resolve()
+}
